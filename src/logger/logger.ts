77dc9f0c423e03b8639118ab26b73f1372b206ea
Vladyslav Marchuk <valdis86@gmail.com>
2020-03-21 19:19:15 +0200

    Added loger

---
+++ src/logger/logger.ts
@@ -0,0 +1,27 @@
+import winston from 'winston';
+import { config } from '../config/config';
+
+export const logger = winston.createLogger({
+  level: 'info',
+
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.align(),
+        winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
+      )
+    })
+  ],
+  exitOnError: false
+});
+
+if (config.environment !== 'development') {
+  logger.add(
+    new winston.transports.File({
+      filename: config.logFile,
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+    })
+  );
+}
